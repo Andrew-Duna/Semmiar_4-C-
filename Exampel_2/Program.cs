59e2bee203e05c1474cbd4c1a272b47bbb91cad4
Andrew Kaban <bolotov963@mail.ru>
2023-10-19 19:43:45 +0500

    Completed the second task

---
+++ Exampel_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12м
+int ReadInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int number1 = ReadInt("Введиете число");
+int result = 0;
+while (number1 > 0)
+{
+    result += number1 % 10;
+    number1 /= 10;
+}
+System.Console.WriteLine(result);
